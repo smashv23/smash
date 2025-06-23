88d5c7522faa0cc05334c06172ee0f78e947c95c
smashv23 <princerwanda2004@gmail.com>
2025-06-23 09:41:48 +0300

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *SILVA SPARK TRANSLATION*
+        const translationMessage = `> *SMASH TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
