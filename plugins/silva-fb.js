944e062331cd67b13803a85fa01204817a4c5646
smashv23 <princerwanda2004@gmail.com>
2025-06-23 09:36:32 +0300

    Update silva-fb.js

--- plugins/silva-fb.js
+++ plugins/silva-fb.js
@@ -31,7 +31,7 @@ cmd({
     const videoUrl = data.data.url;
     await conn.sendMessage(from, {
       video: { url: videoUrl },
-      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By Silva spark 💥 ✅",
+      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By Smash 💥 ✅",
     }, { quoted: m });
 
   } catch (error) {
